cca9ffde104d612d612eaade757f10108f9254af
ali <alharrsiali@gmail.com>
2019-10-19 21:28:59 -0500

    v1.003

--- symbols.py
+++ symbols.py
@@ -1,22 +1,33 @@
 import board as bd
 
 board_s = bd.vars.board
+board_se = bd.vars.board
 alpha = bd.vars.alpha
-
 
 
 for i in range(0, len(alpha)):
     out = alpha[i] + "2"
-    board_se = board_s.replace(out,"👨🏾‍🎤")
+    board_se = board_se.replace(out,"👨🏾‍🎤")
 for i in range(0, len(alpha)):
     out = alpha[i] + "7"
-    board_se = board_s.replace(out,"👨‍🎤")
-print(board_se)
+    board_se = board_se.replace(out,"👨‍🎤")
+
+clean_board = bd.cleaning(board_se)
+print(clean_board)
+
 
 print_from = input("[Move from]:")
 print_to = input("[Move to]:")
 board_s = board_s.replace(str(print_from),"  ")
 board_s = board_s.replace(str(print_to),"👨🏾‍🎤")
+
+for i in range(0, len(alpha)):
+    out = alpha[i] + "2"
+    board_s = board_s.replace(out,"👨🏾‍🎤")
+for i in range(0, len(alpha)):
+    out = alpha[i] + "7"
+    board_s = board_s.replace(out,"👨‍🎤")
+
 
 
 
